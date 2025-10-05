6025c47ac2688f7292004e08759f59ee3054908e
SSDRNG <sofistrezha@gmail.com>
2025-10-05 18:12:53 +0300

    Изменила сложение на вычитание
    см код

--- myCode.py
+++ myCode.py
@@ -1,4 +1,4 @@
 print("Hello, this is a simple mathematic code! Please input two integers numbers")
 a = int(input())
 b = int(input())
-print(f"Adding numbers: {a} + {b} = {a+b}")     
+print(f"Subtraction of numbers: {a} - {b} = {a-b}")     
